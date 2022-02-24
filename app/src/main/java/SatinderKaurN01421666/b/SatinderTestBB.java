1e081434159ee1455f0d71cd411775e0a6943577
Satinder Kaur <satindersukhi3@gmail.com>
2022-02-24 18:20:39 -0500

    "Initial commit + Satinder Kaur"

---
+++ app/src/main/java/SatinderKaurN01421666/b/SatinderTestBB.java
@@ -0,0 +1,14 @@
+package SatinderKaurN01421666.b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SatinderTestBB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
